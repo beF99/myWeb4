650eebfc54fce71e362c2e37df3dcd1d653650d8
c2508738 <finkel_b@mail.tel-aviv.gov.il>
2022-09-29 22:29:27 +0300

    server. js rout

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+// var http = require('http').createServer(app);
+
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/MyWeb4'));
+app.use(express.static(__dirname + '/dist/my-web4'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/MyWeb4/index.html'));
+res.sendFile(path.join(__dirname+'/dist/my-web4/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+// http.listen(3000, function () {
+//     console.log('listening on * localhost:3000');
+// });
