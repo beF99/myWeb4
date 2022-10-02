54e60ceb6e427a2e79165d27ada86b1577857748
c2508738 <finkel_b@mail.tel-aviv.gov.il>
2022-10-02 08:24:17 +0300

    server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
-var http = require('http').createServer(app);
+// var http = require('http').createServer(app);
 
+app.set('port', (process.env.PORT || 5000))
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/my-web4'));
@@ -14,9 +14,13 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/my-web4/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
 
-http.listen(process.env.PORT || 3000, function () {
-    console.log('listening on * localhost:3000');
-});
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+  })
+// Start the app by listening on the default Heroku port
+//  app.listen(process.env.PORT || 8080);
+
+// http.listen(process.env.PORT || 3000, function () {
+//     console.log('listening on * localhost:3000');
+// });
